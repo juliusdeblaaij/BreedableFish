190007b06691b25cc474fa9ae2e5e49b8c6be60c
Julius de Blaaij <julius.de.blaaij@gmail.com>
2019-07-04 19:54:55 +0200

    Initial commit Breedable salmon

---
+++ BreedableFish/gulpfile.js
@@ -0,0 +1,5 @@
+const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v1");
+
+const builder = new MinecraftAddonBuilder("Breedable Fish");
+
+module.exports = builder.configureEverythingForMe();
