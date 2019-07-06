37755351efcad4a94399f4f9f76d7f3984d92763
Julius de Blaaij <julius.de.blaaij@gmail.com>
2019-07-06 11:02:22 +0200

    Added scripting support

--- BreedableFish/gulpfile.js
+++ BreedableFish/gulpfile.js
@@ -1,5 +1,7 @@
 const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v1");
+const BrowserifySupport = require("minecraft-addon-toolchain-browserify");
 
-const builder = new MinecraftAddonBuilder("Breedable Fish");
+const builder = new MinecraftAddonBuilder("BreedableFish");
+builder.addPlugin(new BrowserifySupport());
 
 module.exports = builder.configureEverythingForMe();
