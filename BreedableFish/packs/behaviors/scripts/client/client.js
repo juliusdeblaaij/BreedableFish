9d71a9577974b01230a475aa5b18b1848c059329
Julius de Blaaij <julius.de.blaaij@gmail.com>
2019-07-06 17:08:49 +0200

    Added squid interact components, client retrieving all squids with the breedablefish:captured entity

--- BreedableFish/packs/behaviors/scripts/client/client.js
+++ BreedableFish/packs/behaviors/scripts/client/client.js
@@ -1,16 +1,86 @@
 var clientSystem = client.registerSystem(0, 0);
 const displaychat = "minecraft:display_chat_event";
+
+let globalVars = {};
+
+globalVars.emptyQuery = {};
+globalVars.isFirstTick = false;
+
+//var onCapturedEvent = { narf: false };
+
 // Setup which events to listen for
 clientSystem.initialize = function () {
     // set up your listenToEvents and register client-side components here.
     this.listenForEvent("minecraft:client_entered_world", (eventData) => this.clientJoined(eventData));
+
+    //this.registerComponent("breedablefish:captured", { value: 0 });
+
+    //filtered_query = this.registerQuery();
+    //this.addFilterToQuery(filtered_query, "minecraft:behavior.squid_idle");//breedablefish:captured
+
+    this.registerComponent("breedablefish:captured", { value: 0 });
+
+    globalVars.emptyQuery = this.registerQuery();
 }
 
 
 clientSystem.clientJoined = function (eventData) {
     this.broadcastEvent(displaychat, "§aLoaded...");
+
+    let captured_entities = [];
+    captured_entities = this.getEntitiesFromQuery(globalVars.emptyQuery);
+
+    if (globalVars.isFirstTick === false) {
+
+        if (captured_entities != null && captured_entities.length > 0) {
+            for (let i = 0; i < captured_entities.length; i++) {
+                let captured_entity = this.createEntity();
+                captured_entity = captured_entities[i];
+
+                if (captured_entity.__identifier__ === "minecraft:squid") {
+                    //this.broadcastEvent(displaychat, "§aFound squid");
+
+                    if (this.createComponent(captured_entity, "breedablefish:captured") != null) {
+                        this.broadcastEvent(displaychat, "§a Added component to squid");
+
+                    } else {
+                        this.broadcastEvent(displaychat, "Something went wrong while adding breedablefish:captured component to squid!");
+
+                    }
+                }
+
+            }
+        }
+
+        globalVars.isFirstTick = true;
+    }
 }
 // per-tick updates
 clientSystem.update = function () {
     // Any logic that needs to happen every tick on the client.
+
+    let captured_entities = [];
+    captured_entities = this.getEntitiesFromQuery(globalVars.emptyQuery);
+
+    if (captured_entities != null && captured_entities.length > 0) {
+        for (let i = 0; i < captured_entities.length; i++) {
+            let captured_entity = this.createEntity();
+            captured_entity = captured_entities[i];
+
+            if (captured_entity.__identifier__ === "minecraft:squid") {
+                //this.broadcastEvent(displaychat, "§aFound squid");
+
+                if (this.hasComponent(captured_entity, "breedablefish:captured")) {
+                    this.broadcastEvent(displaychat, "§aFound squid with component");
+
+                }
+            }
+
+            //if (this.hasComponent(captured_entity, "breedablefish:captured")) {
+            //    this.broadcastEvent(displaychat, "§aFound squid with components...");
+
+            //}
+        }
+    }
+    
 }
--- BreedableFish/packs/behaviors/scripts/server/server.js
+++ BreedableFish/packs/behaviors/scripts/server/server.js
@@ -1,11 +1,24 @@
 var serverSystem = server.registerSystem(0, 0);
+const displaychat = "minecraft:display_chat_event";
 
 // Setup which events to listen for
 serverSystem.initialize = function () {
     // set up your listenToEvents and register server-side components here.
+
+    //set up chat event data object
+    
 }
 
 // per-tick updates
 serverSystem.update = function () {
     // Any logic that needs to happen every tick on the server.
+    //set up chat event data object
+    //this.broadcastEvent(displaychat, "§aUpdated...");
+
+    
+}
+
+serverSystem.onCaptured = function (eventData) {
+    //this.broadcastEvent(displaychat, "§eSquid captured...");
+
 }
